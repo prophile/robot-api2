0791b1493d6208e9f444f50e9ab3ae818175922b
Alistair Lynn <arplynn@gmail.com>
2018-07-16 21:54:03 +0100

    Publicise the Robot class

--- robot/__init__.py
+++ robot/__init__.py
@@ -1,8 +1,9 @@
 """Robot API."""
 
 from .motor import MotorDriveSpecialState
+from .robot import Robot
 
 BRAKE = MotorDriveSpecialState.BRAKE
 COAST = MotorDriveSpecialState.COAST
 
-__all__ = ["BRAKE", "COAST"]
+__all__ = ["BRAKE", "COAST", "Robot"]
